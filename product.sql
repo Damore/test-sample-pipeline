56c41ff6f8ec88510bbb9cb701f563fc7cf71585
Gabriel <damore120@gmail.com>
2022-10-04 15:32:35 -0300

    Update product.sql

--- product.sql
+++ product.sql
@@ -1,5 +1,5 @@
 
 SELECT 
     stg.id as chaveprimaria
-    --metadata{customer_connector,product}--
+    --metadata{customer_connector,first_staging}--
 from stg_customer_connector_first_staging as stg
